401f51d3309c41380c73a1d26ca37b87629a9aca
teemaru00 <96940624+joycetea@users.noreply.github.com>
2022-01-01 19:29:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,79 +1,40 @@
+import os
+from datetime import datetime
 
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup
+from flask import Flask, abort, request
 
+# https://github.com/line/line-bot-sdk-python
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-def findCard(cardID : str) -> str: 
-    #cardID = '309-007' #之後代換LINE OCR內容
-    options = Options()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    options.add_argument("--disable-notifications")
-    ser=Service(ChromeDriverManager().install())
-    chrome = webdriver.Chrome(service=ser, options=options)
-    chrome.get("http://220.134.173.17/gameking/card/ocg_show.asp?call_no=" + cardID)
-     
-    strR = ""
-    try:
-        soup = BeautifulSoup(chrome.page_source, 'html.parser')
-        card_query = soup.select('p table')[1].select('tbody tr td')
-        strR += card_query[1].text + '\n'
-        result = strR
-        
-    except Exception as ex:
-        result = '查無此卡'
-        
-    if result == '' or result is None:
-         result = '查無此卡'
-    return result
+app = Flask(__name__)
 
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 29 20:03:37 2021
-
-@author: eugenetseng
-"""
-
-cardInfo = findCard('309-AAA') #傳入卡號 取回卡片效果（str格式）
-print(cardInfo)
+line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
 
+@app.route("/", methods=["GET", "POST"])
+def callback():
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-from selenium.webdriver.common.by import By
+    if request.method == "GET":
+        return "Hello Heroku"
+    if request.method == "POST":
+        signature = request.headers["X-Line-Signature"]
+        body = request.get_data(as_text=True)
+
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            abort(400)
+
+        return "OK"
 
 
-PATH = "./chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    get_message = event.message.text
 
-driver.get("https://www.db.yugioh-card.com/yugiohdb/deck_search.action?request_locale=ja")
-search = driver.find_element_by_name("cardname")
-search.send_keys("マジクリボー") #之後用OCR掃出來的卡名取代這裡的マジクリボー
-search.send_keys(Keys.RETURN)
-
-time.sleep(5)
-
-links = driver.find_elements(By.TAG_NAME, "a")
-
-count = 0
-while count <=29:
-    del links[0]
-    count = count+1
-
-addrs = [0,1,2,3,4]
-count = 0
-while count <= 4:
-    addrs.insert(count,links[0])
-    del links[0]
-    count = count+1
-    
-for addr in addrs:
-    print (addr.get_attribute("href"))
-#addrs是一個list，裡面存了最新分享的五個牌組的網址
-time.sleep(5)
-driver.quit()
+    # Send To Line
+    reply = TextSendMessage(text=f"{get_message}")
+    line_bot_api.reply_message(event.reply_token, reply)
